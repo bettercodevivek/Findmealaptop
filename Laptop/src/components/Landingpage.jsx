13c1e3a6cf2d5bebbf997d68acb8733f019e0991
bettercodevivek <viveksingh0520@gmail.com>
2024-07-23 09:22:45 +0530

    Updation

---
+++ Laptop/src/components/Landingpage.jsx
@@ -0,0 +1,20 @@
+const Landingpage = () =>{
+    return(
+        <div>
+            <h1 className=" relative top-12 text-center text-xl sm:text-3xl md:text-3xl lg:text-4xl">What kind of a laptop are you looking for?</h1>
+         <div className="flex flex-row gap-4 relative top-48 w-full items-center justify-center">
+             <div className="bg-yellow-500">
+               <img src="" className=""/>
+               <p>Gaming Laptop</p>
+             </div>
+             <div className="bg-yellow-500">
+               <img src="" className=""/>
+               <p>Non-Gaming Laptop</p>
+             </div>
+             
+         </div>
+        </div>
+    )
+}
+
+export default Landingpage;
